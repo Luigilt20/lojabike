feb8e5c0250fd4621717089e09036b72e145f34c
Luigi <luigilt029@gmail.com>
2026-08-17 21:59:07 -0300

    feat: Criando declarações e funções da classe

---
+++ lojabike/Bike.cs
@@ -0,0 +1,34 @@
+﻿namespace LojaBike
+{
+    class Bike
+    {
+        public string Modelo { get; set; } = string.Empty;
+        public double bateria { get; set; }
+        public double PesoCiclista { get; set; }
+        public int RecargaMes {  get; set; }
+
+        //Metodos
+
+        public double CalcularAutonomia ()
+        {
+            double autonomiaBase = bateria / 15.0;
+            bool pesoElevado = PesoCiclista > 90.0;
+            //SE O PESO FOR ACIMA DE 90KG A BATERIA REDUZ PARA 15% DE AUTONOMIA
+            return pesoElevado ? (autonomiaBase * 0.05) : autonomiaBase;
+        }
+        public double CalculaConsumoMensal()
+        { 
+            double kmPorCarga = bateria/1000.0
+            return kmPorCarga * RecargaMes;
+        }
+        public double CalcularCustoMensal()
+        { 
+            const double PRECO_KWH = 0.80
+            return CalculaConsumoMensal() * PRECO_KWH;
+        }
+        public bool ModeloEcomico()
+        {
+            return (CalculaConsumoMensal() < 15.0) || (CalcularAutonomia() > 40.0)
+        }
+    }
+}
